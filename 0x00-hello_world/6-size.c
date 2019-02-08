4cf4d409f8f2d334709f53ee35ad2bef2d7910de
Ty Edge <tyedge.inc@gmail.com>
2019-02-08 00:03:41 +0000

    Had to remove a line break

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -21,8 +21,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(\
-		       lld));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lld));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
