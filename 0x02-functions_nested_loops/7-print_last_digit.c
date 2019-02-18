d242f1a85f1b2416c00b83c578ee37a0e6ab3b22
Ty Edge <tyedge.inc@gmail.com>
2019-02-18 10:18:04 +0000

    Correcting my answer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,14 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
-		n *= -1;
-		_putchar((n % 10) + '0');
+		_putchar(n % 10 + '0');
 	}
 	else
 	{
-		_putchar((n % 10) + '0');
+		n *= -1;
+		_putchar(n % 10 + '0');
 	}
 	return (n % 10);
 }
