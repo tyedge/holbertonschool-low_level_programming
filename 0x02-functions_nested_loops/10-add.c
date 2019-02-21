a2c0fb7cd877da1ccb400d6a16d342715a2d6f85
Ty Edge <tyedge.inc@gmail.com>
2019-02-21 22:46:05 +0000

    Adding my answer to the file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - This program adds two
+ * integers and returns the value.
+ * @a: First operand
+ * @b: Second operand
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
