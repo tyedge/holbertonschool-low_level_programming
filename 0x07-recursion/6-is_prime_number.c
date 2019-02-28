23ac6bdceb834d38404b9da25aa2491672a08fbb
Ty Edge <tyedge.inc@gmail.com>
2019-02-28 12:08:15 +0000

    Correcting my answer

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -12,10 +12,16 @@
 int is_prime_number(int n)
 {
 	int div = 2;
+	int t = 10;
 
 	if (n <= 1)
 	{
 		return (0);
+	}
+	else if (n > 5)
+	{
+		if ((n % t) != 1 && (n % t) != 3 && (n % t) != 7 && (n % t) != 9)
+			return (0);
 	}
 	else if (div * div <= n)
 	{
