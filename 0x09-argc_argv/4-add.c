cae8e39747d3421af3be76db8df9da7addff0ea8
Ty Edge <tyedge.inc@gmail.com>
2019-03-05 04:58:48 +0000

    Correcting my answer

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -13,23 +13,22 @@
 
 int main(int argc, char **argv)
 {
-	int i;
+	int i, j;
 	int sum = 0;
 
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (isdigit(*argv[i]))
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				sum += atoi(argv[i]);
+				if (!isdigit(argv[i][j]))
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else
-			{
-				sum = 0;
-				printf("%s\n", "Error");
-				return (1);
-			}
+			sum += atoi(argv[i]);
 		}
 		printf("%d\n", sum);
 	}
