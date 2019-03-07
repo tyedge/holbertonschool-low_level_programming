efa4a2796516535fe9d5919f7946e4bda168ef70
Ty Edge <tyedge.inc@gmail.com>
2019-03-07 03:18:15 +0000

    Updating my answers

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -19,11 +19,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc((size + 1) * sizeof(char));
+	arr = malloc(size * sizeof(char));
 	if  (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 		arr[i] = c;
-	arr[i] = '\n';
 	return (arr);
 }
--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _strlen(char *n);
+
 /**
  * _strdup - returns a pointer to a newly allocated
  * space in memory, which contains a copy of the
@@ -12,4 +14,41 @@
  * or NULL if it fails
  */
 
-char *_strdup(char *str);
+char *_strdup(char *str)
+{
+	char *p;
+	int i, len;
+
+	len = _strlen(str);
+
+	if (len == 0)
+		return (NULL);
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; *str != '\0'; str++, i++)
+	{
+		p[i] = str[0];
+	}
+	p[i++] = '\0';
+	return (p);
+}
+
+/**
+ * _strlen - finds the string length
+ * @n: First operand
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *n)
+{
+	int i;
+
+	for (i = 0; *n != '\0'; n++, i++)
+	{
+		;
+	}
+	return (i);
+}
