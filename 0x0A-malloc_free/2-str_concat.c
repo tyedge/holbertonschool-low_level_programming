3c4b607d232de4fd9b53363072a9502803c2862e
Ty Edge <tyedge.inc@gmail.com>
2019-03-10 06:09:34 +0000

    Correcting my Answer

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -17,9 +17,9 @@ char *str_concat(char *s1, char *s2)
 	int i;
 
 	if (s1 == NULL)
-		s1[0] = '\0';
-	else if (s2 == NULL)
-		s2[0] = '\0';
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	p = malloc(sizeof(char) * (_strlen(s1) + _strlen(s2) + 1));
 
