9ff3a9f49b7339be4c6dabcc502023980d8a7175
Ty Edge <tyedge.inc@gmail.com>
2019-03-12 18:37:30 +0000

    Changed my if statement for Age

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -10,20 +10,23 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+			printf("Name: %p\n", d->name);
+		else
+			printf("Name: %s\n", d->name);
+
+		printf("Age: %.6f\n", d->age);
+
+		if (d->owner == NULL)
+			printf("Owner: %p\n", d->owner);
+		else
+			printf("Owner: %s\n", d->owner);
+	}
+	else
 	{
 		return;
 	}
-	if (d->name == NULL)
-		printf("Name: %p\n", d->name);
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == 0)
-		printf("Age: (nil)\n");
-	else
-       		printf("Age: %.6f\n", d->age);
-	if (d->owner == NULL)
-		printf("Owner: %p\n", d->owner);
-	else
-		printf("Owner: %s\n", d->owner);
+
 }
