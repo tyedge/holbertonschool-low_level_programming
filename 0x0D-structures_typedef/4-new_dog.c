26b89d912326214ffcb8a9ef9493551d05114942
Ty Edge <tyedge.inc@gmail.com>
2019-03-13 01:12:52 +0000

    Updating my answers

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -19,26 +19,87 @@ dog_t *new_dog(char *name, float age, char *owner)
 	doggy = malloc(sizeof(dog_t));
 	if (doggy == NULL)
 		return (NULL);
-	nm = malloc(sizeof(char *));
-	if (nm == NULL)
+	if (name == NULL)
 	{
-		free(doggy);
-		return (NULL);
+		doggy->name = NULL;
 	}
-	nm = name;
-	own = malloc(sizeof(char *));
-	if (own == NULL)
+	else
 	{
-		free(nm);
-		free(doggy);
-		return (NULL);
+		nm = _strdup(name);
+		if (nm == NULL)
+		{
+			free(doggy);
+			return (NULL);
+		}
+		doggy->name = nm;
 	}
-	own = owner;
+	if (owner == NULL)
+	{
+                doggy->owner = NULL;
+        }
+        else
+	{
+		own = _strdup(owner);
+		if (own == NULL)
+		{
+			free(doggy->name);
+			free(doggy);
+			return (NULL);
+		}
+		doggy->owner = own;
+	}
 
-	doggy->name = nm;
 	doggy->age = age;
-	doggy->owner = own;
-
 	return (doggy);
+}
 
+/**
+ * _strdup - returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the
+ * string given as a parameter
+ * @str: string to be duplicated
+ *
+ * Return: pointer to the duplicated string
+ * or NULL if it fails
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i, len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	len = _strlen(str);
+	p = malloc(sizeof(char) * (len + 1));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; *str != '\0'; str++, i++)
+	{
+		p[i] = str[0];
+	}
+	p[i++] = '\0';
+	return (p);
+}
+
+/**
+ * _strlen - finds the string length
+ * @n: First operand
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *n)
+{
+	int i;
+
+	for (i = 0; *n != '\0'; n++, i++)
+	{
+		;
+	}
+	return (i);
 }
