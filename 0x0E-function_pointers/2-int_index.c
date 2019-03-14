dac4bb40ae473f279615555beb05f2bbb3f57c6c
Ty Edge <tyedge.inc@gmail.com>
2019-03-14 01:01:38 +0000

    Changed exit value and Comments

--- 0x0E-function_pointers/2-int_index.c
+++ 0x0E-function_pointers/2-int_index.c
@@ -3,15 +3,10 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches the array passed
- * in as a parameter using the pointer to the
- * cmp function which is also passed in as a
- * parameter and takes one integer argument
+ * int_index - searches for an integer
  * @array: Array on which function will execute
  * @size: Size of the array
- * @cmp: Pointer to a function which takes one
- * integer, is used to compare values, and
- * returns an integer
+ * @cmp: Pointer to a function
  *
  * Return: first array index which matches the
  * item searched for OR -1 if no match is found
@@ -22,11 +17,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i, ret;
 
 	if (array == NULL)
-                exit(99);
+		exit(1);
 	if (size == 0)
 		return (-1);
 	if (cmp == NULL)
-		exit(99);
+		exit(1);
 	for (i = 0; i < size; i++)
 	{
 		ret = cmp(array[i]);
