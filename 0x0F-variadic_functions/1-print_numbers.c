3c2b663dedee6b2b3f3ca0759d20f6a80ca780f6
Ty Edge <tyedge.inc@gmail.com>
2019-03-16 20:50:03 +0000

    Correcting my answer

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -26,14 +26,23 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (separator != NULL && *separator == '\0' && i != (n - 1))
-			printf("%i ", va_arg(nums, unsigned int));
-		if (separator != NULL && *separator != '\0' && i != (n - 1))
-			printf("%i%s", va_arg(nums, unsigned int), separator);
-		if (separator == NULL && i != (n - 1))
-			printf("%i ", va_arg(nums, unsigned int));
-		if (i == (n - 1))
-			printf("%i\n", va_arg(nums, unsigned int));
+		if (separator != NULL)
+
+		{
+			if (*separator == '\0' && i != (n - 1))
+				printf("%i ", va_arg(nums, unsigned int));
+			else if (i != (n - 1))
+				printf("%i%s", va_arg(nums, unsigned int), separator);
+			else
+				printf("%i\n", va_arg(nums, unsigned int));
+		}
+		if (separator == NULL)
+		{
+			if (i != (n - 1))
+				printf("%i ", va_arg(nums, unsigned int));
+			else
+				printf("%i\n", va_arg(nums, unsigned int));
+		}
 	}
 	va_end(nums);
 }
