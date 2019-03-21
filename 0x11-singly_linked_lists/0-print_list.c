5e3ea53d3d006c547900d25644f9d7c98f735854
Ty Edge <tyedge.inc@gmail.com>
2019-03-21 16:30:31 +0000

    Adding answers to files

--- 0x11-singly_linked_lists/0-print_list.c
+++ 0x11-singly_linked_lists/0-print_list.c
@@ -1,8 +1,13 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
 
+/**
+ * print_list - prints all elements of a
+ * linked list
+ * @h: pointer to the first element of the list
+ *
+ * Return: the number of nodes
+ *
+ */
 
 size_t print_list(const list_t *h)
 {
