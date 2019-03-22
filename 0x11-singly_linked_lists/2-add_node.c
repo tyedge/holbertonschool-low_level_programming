3a707c0a04ba7a53b9be9d66a34b5b337dbce24c
Ty Edge <tyedge.inc@gmail.com>
2019-03-22 00:05:03 +0000

    Adding answer to file

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node to the beginning  of
+ * a linked list
+ * @head: a pointer to a pointer to a list_t struct
+ * @str: a string value to be added to the list
+ *
+ * Return: the number of nodes
+ *
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t **other_nm = head;
+	list_t *newer;
+	unsigned int i = 0;
+
+	newer = malloc(sizeof(list_t));
+	if (newer == NULL)
+		return (NULL);
+	newer->str = strdup(str);
+	while (newer->str[i] != '\0')
+	{
+		i++;
+	}
+	newer->len = i;
+	newer->next = *other_nm;
+	*other_nm = newer;
+
+	return (newer);
+}
