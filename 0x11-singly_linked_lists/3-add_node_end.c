84f2393696a5a58b38d34be47bef0a6ea91f6802
Ty Edge <tyedge.inc@gmail.com>
2019-03-22 03:26:43 +0000

    Adding answer to file

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+
+/**
+ * add_node_end - adds a node to the end of a
+ * linked list
+ * @head: a pointer to a pointer to a list_t struct
+ * @str: a string value to be added to the list
+ *
+ * Return: pointer to the new element's address
+ *
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t **other_nm = head;
+	list_t *temp;
+	list_t *newer;
+	unsigned int i = 0;
+
+	newer = malloc(sizeof(list_t));
+	if (newer == NULL)
+	return (NULL);
+	newer->str = strdup(str);
+	newer = malloc(sizeof(list_t));
+	if (newer == NULL)
+		return (NULL);
+	newer->str = strdup(str);
+	if (newer->str != '\0')
+	{
+		while (newer->str[i] != '\0')
+		{
+			i++;
+		}
+	}
+	newer->len = i;
+	newer->next = NULL;
+	if (*other_nm == NULL)
+	{
+		*other_nm = newer;
+	}
+	else
+	{
+		temp = *other_nm;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newer;
+	}
+	return (newer);
+}
