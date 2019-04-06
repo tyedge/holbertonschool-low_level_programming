b311fa4a3621d02def4e552a828ae3d1849809ae
Ty Edge <tyedge.inc@gmail.com>
2019-04-06 23:35:32 +0000

    Correcting answer

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -21,12 +21,18 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file0 = open(filename, O_WRONLY | O_APPEND);
+	file0 = open(filename, O_RDWR | O_APPEND);
 	if (file0 < 0)
 		return (-1);
 
-	if (text_content != NULL)
-		write(file0, text_content, sizeof(text_content));
+	if (text_content == NULL)
+		return (1);
+
+	while (*text_content)
+	{
+		write(file0, (const void *)text_content, 1);
+		text_content++;
+	}
 
 	close(file0);
 
