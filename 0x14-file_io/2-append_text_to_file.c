dfb79083fc67b4a7a687c39207e188bc4d6515f7
Ty Edge <tyedge.inc@gmail.com>
2019-04-06 23:02:55 +0000

    Submitting changes

--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -25,10 +25,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file0 < 0)
 		return (-1);
 
-	if (text_content == NULL)
-		return (1);
-
-	write(file0, text_content, sizeof(text_content));
+	if (text_content != NULL)
+		write(file0, text_content, sizeof(text_content));
 
 	close(file0);
 
