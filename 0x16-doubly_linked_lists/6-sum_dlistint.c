e2fe3a4efbcc1b4c9cdf829d49ee273af25658b4
tyedge <tyedge.inc@gmail.com>
2019-05-04 20:12:05 +0000

    Adding answers to files

---
+++ 0x16-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data
+ * (n) of a dlistint_t linked list
+ * @head: pointer to the first element of the list
+ *
+ * Return: the number of nodes
+ *
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
