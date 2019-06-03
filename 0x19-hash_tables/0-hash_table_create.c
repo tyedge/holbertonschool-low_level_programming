f6e3ce88eb94814b5d8ac407005ca9badc9f83f3
tyedge <tyedge.inc@gmail.com>
2019-06-03 19:48:57 +0000

    Resubmitting

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
 
-
 /**
  * hash_table_create - this program creates a new hash table
- * @size - the size of the array
+ * @size: the size of the array
  *
  * Return: a pointer to the newly created hash table or NULL
  */
@@ -11,16 +10,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *newtable;
-	hash_node_t *newarr;
 
 	newtable = calloc(size, sizeof(hash_table_t *));
 	if (newtable == NULL)
 	{
-		free(newtable);
 		return (NULL);
 	}
-	newtable->size = size;
-	newtable->array = &newarr;
 
 	return (newtable);
 }
