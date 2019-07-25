6997f777c55feebef0aa20399ba09699b56ba2bd
tyedge <tyedge.inc@gmail.com>
2019-07-25 08:32:29 +0000

    Removed NULL pointer checks

--- 0x1D-binary_trees/8-binary_tree_postorder.c
+++ 0x1D-binary_trees/8-binary_tree_postorder.c
@@ -8,18 +8,13 @@
  *
  * Return: void
  */
+
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (tree == NULL)
-		return;
-	if (func == NULL)
-		return;
-
 	if (tree != NULL)
 	{
 		binary_tree_postorder(tree->left, func);
 		binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
-
 }
